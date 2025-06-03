e4ae905d58eb6333863ff29906654d3e7ee8c69b
Alex-GF <yalejandro9@gmail.com>
2025-06-03 19:51:14 +0200

    docs: usePricingToken

--- src/main/hooks/usePricingToken.ts
+++ src/main/hooks/usePricingToken.ts
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { SpaceContext } from '../contexts/SpaceContext';
 
 /**
- * Custom hook to access the SpaceClient instance from context.
+ * Custom hook to access the service that manages the pricing token.
  * Throws an error if used outside of SpaceProvider.
  */
 export function usePricingToken() {
