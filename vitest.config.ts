2a6310a87aa6e8ea4e00eed2dbfae81525455f4a
Alex-GF <yalejandro9@gmail.com>
2025-06-03 10:00:06 +0200

    fix: test alias resolution

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(process.cwd(), 'src/main'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
